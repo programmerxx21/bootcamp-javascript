7a35ee3cd5c18d6c2bf8bdd95822fec2833ae6b5
mrdmssin21 <mrdmssin@gmail.com>
2022-11-07 12:11:43 +0100

    subida1
    subida mi primer proyecto node

---
+++ 1. introducción.js
@@ -0,0 +1,10 @@
+/*
+Existen 2 tipos de lenguaje (compilado e interpretado)
+Diferencias:
+    - El lenguaje interpretado no necesita ser compilado, es como una especie de guión que un intérprete (navegador), va ejecutando línea por línea.
+    - Un lenguaje compilado es un lenguaje en el que tú desarrollas tu programa, después, pasaría por un punto intermedio, donde compilaríamos nuestro programa (se traduce a lenguaje binario). Para que después una máquina, lo pueda lanzar.
+
+Nosotros utilizaremos un lenguaje interpretado (JavaScript o JS)
+- Utilizamos JS para que nuestras páginas web sean interactivas.
+- JS es un lenguaje que lo utilizan diferentes navegadores, sin embargo; existe una entidad llamada ECMA que se encarga de estandarizar lenguajes de programación. Es simplemente una entidad reguladora que estandariza un lenguaje de programación en todos o casi todos los navegadores.
+*/
---
+++ 2. node js y npm.js
@@ -0,0 +1,7 @@
+/*
+- JS se creo principalmente para navegadores web.
+- Node JS es un entoro basado en JS que es capaz de ser ejecutado a través de cualquier plataforma, como windows, mac, android, servidores web...
+
+NPM (Node package manager)
+- Se encarga de gestionar todos los paquetes que tengan que ver con el entorno de node JS.
+*/
